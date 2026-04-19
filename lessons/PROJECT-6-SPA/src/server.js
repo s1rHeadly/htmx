6681eb4fb94bc027809a50cad7b25656e34681a7
davidmchale <dmchale@squiz.net>
2026-04-19 12:58:23 +1000

    creating the first api endpoint with hono

--- lessons/PROJECT-6-SPA/src/server.js
+++ lessons/PROJECT-6-SPA/src/server.js
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import BOOKS_DATA from "../data/bookdata.js";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hono is running ✅");
+app.get("/books", (c) => {
+  // this will point to http://localhost:3000/books
+  console.log("Books route hit");
+  return c.json(BOOKS_DATA);
 });
 
 serve({
